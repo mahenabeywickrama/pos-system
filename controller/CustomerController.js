a55bea0359f101189cb14d6a534d8efca9badeba
mahenabeywickrama <mahenabeywickrema2003@gmail.com>
2025-05-09 18:16:49 +0530

    added customer crud

---
+++ controller/CustomerController.js
@@ -0,0 +1,118 @@
+import {customer_db} from "/db/db.js";
+import CustomerModel from "/model/CustomerModel.js";
+
+let selectedIndex = -1;
+
+function resetCustomerForm() {
+    $('#cusid, #fname, #lname, #email, #phone, #address').val('');
+    selectedIndex = -1;
+}
+
+function loadCustomers() {
+    const tbody = $('#customer-tbody');
+    tbody.empty();
+
+    customer_db.forEach((item) => {
+        let data = `<tr data-id="${item.id}">
+                        <td>${item.id}</td>
+                        <td>${item.fname}</td>
+                        <td>${item.lname}</td>
+                        <td>${item.address}</td>
+                        <td>${item.email}</td>
+                        <td>${item.phone}</td>
+                    </tr>`;
+        tbody.append(data);
+    });
+}
+
+$('#customer_save').on('click', function(){
+    let id = $('#cusid').val().trim();
+    let fname = $('#fname').val().trim();
+    let lname = $('#lname').val().trim();
+    let address = $('#address').val().trim();
+    let email = $('#email').val().trim();
+    let phone = $('#phone').val().trim();
+
+    if (!id || !fname || !lname || !address) {
+        Swal.fire({title: "Error!", text: "Please fill required fields", icon: "error"});
+        return;
+    }
+
+    if (customer_db.some(c => c.id === id)) {
+        Swal.fire({title: "Error!", text: "Customer ID already exists!", icon: "error"});
+        return;
+    }
+
+    const customer = new CustomerModel(id, fname, lname, address, email, phone);
+    customer_db.push(customer);
+
+    loadCustomers();
+    resetCustomerForm();
+
+    Swal.fire({title: "Added Successfully!", icon: "success"});
+});
+
+$('#customer_update').on('click', function () {
+    if (selectedIndex === -1) {
+        Swal.fire({title: "No Selection!", text: "Please select a customer to update.", icon: "info", confirmButtonText: "Ok"})
+        return;
+    }
+
+    let id = $('#cusid').val();
+    let fname = $('#fname').val();
+    let lname = $('#lname').val();
+    let address = $('#address').val();
+    let email = $('#email').val();
+    let phone = $('#phone').val();
+
+    customer_db[selectedIndex] = new CustomerModel(id, fname, lname, address, email, phone);
+
+    loadCustomers();
+    resetCustomerForm();
+
+    Swal.fire({title: "Updated Successfully!", icon: "success", draggable: true});
+})
+
+$('#customer_delete').on('click', function () {
+    if (selectedIndex === -1) {
+        Swal.fire({title: "No Selection!", text: "Please select a customer to delete.", icon: "info", confirmButtonText: "Ok"})
+        return;
+    }
+
+    Swal.fire({
+        title: 'Are you sure?',
+        text: 'This customer will be removed!',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, delete it!'
+    }).then(result => {
+        if (result.isConfirmed) {
+            customer_db.splice(selectedIndex, 1);
+            loadCustomers();
+            resetCustomerForm();
+
+            Swal.fire({title: "Deleted Successfully!", icon: "success"});
+        }
+    });
+});
+
+
+$('#customer_reset').on('click', resetCustomerForm);
+
+$("#customer-tbody").on('click', 'tr', function(){
+    const clickedId = Number($(this).data('id')).toString();
+    const obj = customer_db.find(c => c.id === clickedId);
+    selectedIndex = customer_db.findIndex(c => c.id === clickedId);
+
+    console.log(clickedId)
+    console.log(obj)
+
+    if (!obj) return;
+
+    $("#cusid").val(obj.id);
+    $("#fname").val(obj.fname);
+    $("#lname").val(obj.lname);
+    $("#address").val(obj.address);
+    $("#email").val(obj.email);
+    $("#phone").val(obj.phone);
+});
---
+++ model/CustomerModel.js
@@ -0,0 +1,10 @@
+export default class CustomerModel {
+    constructor(id, fname, lname, address, email, phone) {
+        this.id = id;
+        this.fname = fname;
+        this.lname = lname;
+        this.address = address;
+        this.email = email;
+        this.phone = phone;
+    }
+}
